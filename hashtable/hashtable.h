f8fb6c62fb885be6fddea3b07b47103c84a0d608
Lorenz Hübschle-Schneider <lorenzhs@ira.uka.de>
2015-05-19 15:31:14 +0200

    Add hashtable interface and (untested) std::unordered_map wrapper

---
+++ hashtable/hashtable.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../common/maybe.h"
+
+using namespace common::maybe;
+
+template <typename Key, typename T>
+class hashtable {
+public:
+    using value_type = std::pair<Key, T>;
+
+    /// Retrieve a key's value, or insert a default-constructed value if not found
+    virtual T& operator[](const Key &key) = 0;
+
+    /// Retrieve a key's value by rvalue reference, or insert a default-constructed value if not found
+    virtual T& operator[](Key &&key) = 0;
+    
+    /// Find a key in the hash table
+    virtual maybe<T> find(const Key &key) = 0;
+
+    /// Erases all elements with the given key
+    /// Returns the number of elements removed
+    virtual size_t erase(const Key &key) = 0;
+
+    /// Returns the number of elements
+    size_t size() const = 0;
+
+    /// Clear the hash table
+    virtual void clear() = 0;
+};
---
+++ hashtable/unordered_map.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#pragma once
+
+#include <unordered_map>
+
+#include "hashtable.h"
+
+namespace hashtable {
+    
+template <typename Key,
+          typename T,
+          typename Hash = std::hash<Key>,
+          typename KeyEqual = std::equal_to<Key>,
+          typename Allocator = std::allocator< std::pair<const Key, T>>
+class unordered_map : public hashtable<Key, T> {
+public:
+    unordered_map(const size_t bucket_count = 0) : hashtable(), map(bucket_count) {}
+    ~unordered_map() {}
+
+    T& operator[](const Key &key) {
+        return map[key];
+    }
+
+    T& operator[](Key&& key) {
+        return map[std::forward<Key>(key)];
+    }
+
+    maybe<T> find(const Key &key) {
+        auto it = map.find(key);
+        if (it == map.end()) {
+            return nothing<T>();
+        } else {
+            return just<T>(*it);
+        }
+    }
+
+    size_t erase(const Key &key) {
+        return map.erase(key);
+    }
+
+    size_t size() const { return map.size(); }
+
+    void clear() { map.clear(); }
+
+protected:
+    std::unordered_map<Key, T, Hash, KeyEqual, Allocator> map;
+};
+
+};
