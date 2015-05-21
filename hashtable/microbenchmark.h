e901752947c06010ee04f2dfcb53b27f8f266916
Lorenz Hübschle-Schneider <lorenzhs@ira.uka.de>
2015-05-21 18:10:36 +0200

    Add hashtable microbenchmark dummy

---
+++ hashtable/microbenchmark.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "../common/timer.h"
+
+namespace hashtable {
+
+template <typename HashTable>
+class microbenchmark {
+public:
+	using Benchmark = common::benchmark<HashTable>;
+	using BenchmarkFactory = common::contender_factory<Benchmark>;
+	common::contender_list<Benchmark> benchmarks;
+
+	static void register_benchmarks(common::contender_list<Benchmark> &benchmarks) {
+		auto fill = [](HashTable &&map) {
+			const int num = 1000000;
+			for (size_t i = 0; i < num; ++i) {
+				map[i] = num - i;
+			}
+		};
+
+		common::register_benchmark("insert", fill, benchmarks);
+
+		common::register_benchmark("insert+find", [](HashTable &&map) {
+			const int num = 1000000;
+			for (int i = 0; i < num; ++i) {
+				map[i] = i; // TODO randomize
+			}
+			int sum = 0;
+			for (int i = 0; i < num; ++i) {
+				sum += map.find(i); // TODO randomize
+			}
+			return sum;
+		}, benchmarks);
+
+
+		common::register_benchmark("ins-del-ins + del-ins-del", [](HashTable &&map) {
+			const int num = 1000000;
+			for (size_t i = 0; i < num; ++i) {
+				// TODO randomize
+				map[i] = i;
+				map.erase(i);
+				map[i] = num-i;
+			}
+			for (size_t i = 0; i < num; ++i) {
+				map.erase(i);
+				map[i] = i;
+				map.erase(i);
+			}
+		}, benchmarks);
+
+		common::register_benchmark("access", fill,	[](HashTable &&map) {
+			const int num = 1000000;
+			for (size_t i = 0; i < num; ++i) {
+				(void)map[i];
+			}
+		}, benchmarks);
+
+		common::register_benchmark("find", fill, [](HashTable &&map) {
+			const int num = 1000000;
+			for (size_t i = 0; i < num; ++i) {
+				(void)map.find(i);
+			}
+		}, benchmarks);
+	}
+};
+}
