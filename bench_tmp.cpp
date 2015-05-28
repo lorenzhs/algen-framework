d916f67abb317ffd79da4aeb150f3e5a5338113a
Lorenz Hübschle-Schneider <lorenzhs@ira.uka.de>
2015-05-28 14:24:50 +0200

    Add configuration of instrumentations

--- bench_tmp.cpp
+++ bench_tmp.cpp
@@ -24,17 +24,26 @@ void usage(char* name) {
 		 << "-p <prefix>   result filename prefix (default: results_)" << endl
 		 << "-n <int>      number of repetitions for each benchmark (default: 1)" << endl
 		 << "-c <double>   cutoff, at which difference ratio to stop printing (deafult: 1.01)" << endl
-		 << "-m <int>      maximum number of differences to print (default: 25)" << endl;
+		 << "-m <int>      maximum number of differences to print (default: 25)" << endl
+		 << endl
+		 << "Instrumentation options:" << endl
+		 << "-nt           disable timer instrumentation" << endl
+		 << "-np           disable all PAPI instrumentations" << endl
+		 << "-npc          disable PAPI cache instrumentation" << endl
+		 << "-npi          disable PAPI instruction instrumentation" << endl;
 	exit(0);
 }
 
 int main(int argc, char** argv) {
 	common::arg_parser args(argc, argv);
 	if (args.is_set("h") || args.is_set("-help")) usage(argv[0]);
-	std::string resultfn_prefix = args.get<std::string>("p", "results_");
-	size_t repetitions = args.get<size_t>("n", 1),
-		   max_results = args.get<size_t>("m", 25);
-	double cutoff = args.get<double>("c", 1.01);
+	const std::string resultfn_prefix = args.get<std::string>("p", "results_");
+	const size_t repetitions = args.get<size_t>("n", 1),
+		         max_results = args.get<size_t>("m", 25);
+	const double cutoff = args.get<double>("c", 1.01);
+	const bool disable_timer      = args.is_set("nt"),
+			   disable_papi_cache = args.is_set("npc") || args.is_set("np"),
+			   disable_papi_instr = args.is_set("npi") || args.is_set("np");
 
 	using HashTable = hashtable::hashtable<int, int>;
 	using Benchmark = common::benchmark<HashTable, size_t>;
@@ -51,12 +60,15 @@ int main(int argc, char** argv) {
 	// Register instrumentations
 	common::contender_list<common::instrumentation> instrumentations;
 #ifndef MALLOC_INSTR
+	if (!disable_timer)
 	instrumentations.register_contender("timer", "timer",
 		[](){ return new common::timer_instrumentation(); });
 
+	if (!disable_papi_cache)
 	instrumentations.register_contender("PAPI cache", "PAPI_cache",
 		[](){ return new common::papi_instrumentation_cache(); });
 
+	if (!disable_papi_instr)
 	instrumentations.register_contender("PAPI instruction", "PAPI_instr",
 		[](){ return new common::papi_instrumentation_instr(); });
 #else
