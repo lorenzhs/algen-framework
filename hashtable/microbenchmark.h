16e47b21f34c3b439839b2d1830faa103b183ffc
Lorenz Hübschle-Schneider <lorenzhs@ira.uka.de>
2015-05-29 14:15:59 +0200

    Adapt hashtable microbenchmarks to new interface

--- hashtable/microbenchmark.h
+++ hashtable/microbenchmark.h
@@ -16,17 +16,18 @@ public:
     common::contender_list<Benchmark> benchmarks;
 
     static void register_benchmarks(common::contender_list<Benchmark> &benchmarks) {
-        auto fill = [](HashTable &map, size_t num) {
+        auto fill = [](HashTable &map, size_t num, void*) {
             for (size_t i = 1; i <= num; ++i) {
                 map[i] = num - i;
             }
+            return nullptr;
         };
 
         const std::vector<size_t> sizes{1<<16, 1<<18, 1<<20, /*1<<22, 1<<24, 1<<26*/};
 
         common::register_benchmark("insert", "insert",  fill, sizes, benchmarks);
 
-        common::register_benchmark("insert+find", "insert-find", [](HashTable &map, size_t num) {
+        common::register_benchmark("insert+find", "insert-find", [](HashTable &map, size_t num, void*) {
             for (size_t i = 1; i <= num; ++i) {
                 map[i] = i; // TODO randomize
             }
@@ -36,7 +37,7 @@ public:
         }, sizes, benchmarks);
 
 
-        common::register_benchmark("ins-del-ins + del-ins-del", "ins-del-cycle", [](HashTable &map, size_t num) {
+        common::register_benchmark("ins-del-ins + del-ins-del", "ins-del-cycle", [](HashTable &map, size_t num, void*) {
             for (size_t i = 1; i <= num; ++i) {
                 // TODO randomize
                 map[i] = i;
@@ -50,13 +51,13 @@ public:
             }
         }, sizes, benchmarks);
 
-        common::register_benchmark("access", "access", fill, [](HashTable &map, size_t num) {
+        common::register_benchmark("access", "access", fill, [](HashTable &map, size_t num, void*) {
             for (size_t i = 1; i <= num; ++i) {
                 (void)map[i];
             }
         }, sizes, benchmarks);
 
-        common::register_benchmark("find", "find", fill, [](HashTable &map, size_t num) {
+        common::register_benchmark("find", "find", fill, [](HashTable &map, size_t num, void*) {
             for (size_t i = 1; i <= num; ++i) {
                 (void)map.find(i);
             }
