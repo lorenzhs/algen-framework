af5c768285bf9728d13238b45d98ff39e2c513c1
Lorenz Hübschle-Schneider <lorenzhs@ira.uka.de>
2015-05-29 10:57:00 +0200

    Add priority queue interface

---
+++ pq/priority_queue.h
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace pq {
+
+template <typename T>
+class priority_queue {
+public:
+    // You also need to provide the following:
+    // static void register_contenders(common::contender_list<priority_queue<T>> &list)
+
+    /// Add an element to the priority queue by const lvalue reference
+    virtual void push(const T& value) = 0;
+    /// Add an element to the priority queue by rvalue reference (with move)
+    virtual void push(T&& value) = 0;
+
+    /// Deletes the top element
+    virtual void pop() = 0;
+
+    /// Retrieves the top element
+    virtual const T& top() = 0;
+
+    /// Get the number of elements in the priority queue
+    virtual size_t size() = 0;
+
+    /// Virtual destructor needed for inheritance
+    virtual ~priority_queue() {}
+};
+
+}
