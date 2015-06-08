b3e341a5f848e08e6c11673386d13e0a3cf3aa45
Lorenz Hübschle-Schneider <lorenzhs@ira.uka.de>
2015-06-08 14:36:56 +0200

    Autogenerate plots
    - Split each benchmark into a separate plot
    -> #Benchmarks x #Instrumentations many plots for {Hashtable, PQ}

--- plots/README.md
+++ plots/README.md
@@ -1,3 +1,16 @@
 # Plots
 
-Die Plots sind work in progress, aber die grundlegende Idee ist erkennbar. Die von `test_hash` und `test_pq` geschriebenen Ergebnisdateien werden von [SqlPlotTools](https://github.com/bingmann/sqlplot-tools) geparsed, und die SQL-Queries in den Plot-Dateien visualisieren einen Aspekt der Daten.
+Die von `test_hash` und `test_pq` geschriebenen Ergebnisdateien werden von [SqlPlotTools](https://github.com/bingmann/sqlplot-tools) geparsed, und die SQL-Queries in den Plot-Dateien visualisieren einen Aspekt der Daten.
+
+# Usage
+
+```bash
+cp ../results*.txt .
+make all
+```
+
+tut das Richtige™ und generiert Plots für alle Experimente, für die Daten vorliegen. Falls auch Plots, für die (noch) keine Daten vorliegen, generiert werden sollen, hilft `./generate.py --all`.
+
+# TODO
+
+HTML-Seite, auf der die Plots in Kategorien unterteilt sind
---
+++ plots/generate.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import os
+import sys
+
+gnuplot_file = """# IMPORT-DATA results {data_fn}
+
+# set terminal pdf size 13.33cm,10cm linewidth 2.0
+set terminal svg enhanced size 1024,768 fname 'Verdana' fsize 10 mouse jsdir "."
+set output "{out_fn}.svg"
+
+set pointsize 0.5
+set grid xtics ytics
+set offsets 0,0,graph 0.01,graph 0.01
+
+set key {keypos}
+
+set title '{ds_name} {bench_name}, {plot_desc}'
+set xlabel 'log₂(n)'
+set ylabel '{plot_desc} per element (n){add}'
+
+{logscale}
+
+## MULTIPLOT(ds) SELECT log(2, config_1) as x, avg({col_name})*1.0/(config_1*{factor}) AS y, MULTIPLOT
+## FROM results WHERE bench="{bench_col}" GROUP BY MULTIPLOT,x ORDER BY MULTIPLOT,x
+"""
+
+plot_types = [
+    ("timer", [("running time", "time", "time", {"factor": 1e6, "add": "in nanoseconds"})]),
+    ("PAPI_cache", [("L1 data cache misses", "L1miss", "L1D_cache_misses", {"keypos": "top left"}),
+                    ("L2 data cache misses", "L2miss", "L2D_cache_misses", {}),
+                    ("L3 cache misses",      "L3miss", "L3_cache_misses", {})]),
+    ("PAPI_instr", [("total cycles", "cycles", "Total_cycles", {}),
+                    ("instructions completed", "instructions", "Instr_completed", {}),
+                    ("branch mispredictions", "branchmiss", "Cond_br_mspredictd", {})]),
+    ("memory", [("number of allocations", "mem_mallocs", "mallocs", {"logscale": False}),
+                ("total memory allocated", "mem_total", "totalmem", {"logscale": False}),
+                ("peak memory usage", "mem_peak", "peakmem", {"logscale": False})])
+]
+
+# Name, Abbrevitation, Benchmarks=[(name, col)]
+ds_types = [("Hash Table", "hash", [("insert", "insert"),
+                                    ("insert+find", "insert-find"),
+                                    ("access", "access"),
+                                    ("find", "find"),
+                                    ("find random", "find-random"),
+                                    ("(ins-del-ins)^n (del-ins-del)^n", "ins-del-cycle"),
+                                    ("wordcount", "wordcount")]),
+            ("Priority Queue", "pq", [("heapsort random", "heapsort-rand"),
+                                      ("heapsort permutation", "heapsort-perm"),
+                                      ("(push-pop-push)^n (pop-push-pop)^n", "idi^n-did^n"),
+                                      ("pop", "pop"),
+                                      ("push", "push"),
+                                      ("push-pop-mix on full heap", "push-pop-mix")])]
+
+data_fn_pattern = "results_{ds}_{res}.txt"
+out_fn_pattern = "{ds}_{bench}_{plot}"
+
+check_existence = True
+if len(sys.argv) > 1:
+    if sys.argv[1] in ["--all", "-a"]:
+        check_existence = False
+
+for (ds_name, ds_abbrv, ds_bench) in ds_types:
+    for (bench_name, bench_col) in ds_bench:
+        for (fn_segment, plots) in plot_types:
+            data_fn = data_fn_pattern.format(ds=ds_abbrv, res=fn_segment)
+
+            # Check whether data file exists
+            if check_existence and not os.path.isfile(data_fn):
+                continue
+
+            for (plot_desc, plot_name, col_name, opts) in plots:
+                out_fn_base = out_fn_pattern.format(ds=ds_abbrv, bench=bench_col, plot=plot_name)
+
+                # Parse options
+                add = opts["add"] if "add" in opts else ""
+                factor = float(opts["factor"]) if "factor" in opts else 1.0
+                keypos = opts["keypos"] if "keypos" in opts else "bottom left"
+
+                if "logscale" in opts and opts["logscale"] in [0, False]:
+                    logscale = ""
+                else:
+                    logscale = "set logscale y"
+
+                # Fill in GNUplot file
+                plotfile = gnuplot_file.format(data_fn=data_fn, out_fn=out_fn_base,
+                    keypos=keypos, ds_name=ds_name, bench_name=bench_name,
+                    plot_desc=plot_desc, add=add, logscale=logscale,
+                    bench_col=bench_col, col_name=col_name, factor=factor)
+
+                with open(out_fn_base + '.plot', 'w') as outfile:
+                    outfile.write(plotfile)
